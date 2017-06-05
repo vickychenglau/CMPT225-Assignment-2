68013e516623ea4eb663171d7483792fe32eb92d
vcl2 <vcl2@sfu.ca>
2017-06-05 13:57:19 -0700

    everything else

---
+++ Patient.cpp
@@ -0,0 +1,145 @@
+/*
+ * Patient.cpp
+ * 
+ * Class Description: Models a walk-in clinic patient.
+ * Class Invariant: Each patient has a unique care card number.
+ *                  This care card number must have 10 digits.
+ *                  This care card number cannot be modified. 
+ *
+ * Last modified on: May 29, 2017
+ * Author: Vicky Lau
+ */
+
+#include <string>
+#include <iostream>		// For printing the data
+#include "List.h"
+#include "Patient.h"	// Header file for this class
+
+using namespace std;
+
+// Default Constructor
+// Description: Create a patient with a care card number of "0000000000". 
+// Postcondition: All data members set to "To be entered", 
+//                except the care card number which is set to "0000000000".       
+Patient::Patient() : name("To be entered"),
+					 address("To be entered"),
+					 phone("To be entered"),
+					 email("To be entered"),
+					 careCard("0000000000") {
+
+
+}
+
+// Parameterized Constructor
+// Description: Create a patient with the given care card number.
+// Postcondition: If aCareCard does not have 10 digits, then care card is set to "0000000000".
+//                All other data members set to "To be entered".
+Patient::Patient(string aCareCard){
+	name    = "To be entered";
+	address = "To be entered";
+	phone   = "To be entered";
+	email   = "To be entered";
+
+	int count      = 0;
+	bool numerical = true;
+
+	while( aCareCard[count] != 0 ){
+		if( aCareCard[count] < '0' || aCareCard[count] > '9' ){
+			numerical = false;
+		}
+		
+		count++;
+	}
+
+	if( count == 10 && numerical ){
+		careCard = aCareCard;
+	}
+	else{
+		careCard = "0000000000";
+	}
+}
+
+// Getters and setters
+// Description: Returns patient's name.
+string Patient::getName() const{
+	return name;
+}
+
+// Description: Returns patient's address.
+string Patient::getAddress() const{
+	return address;
+}
+
+// Description: Returns patient's phone number.
+string Patient::getPhone() const{
+	return phone;
+}
+
+// Description: Returns patient's email.
+string Patient::getEmail() const{
+	return email;
+}
+	
+// Description: Returns patient's care card number.
+string Patient::getCareCard() const{
+	return careCard;
+}
+
+// Description: Sets the patient's name.
+void Patient::setName(const string aName){
+	name = aName;
+}
+
+// Description: Sets the patient's address.
+void Patient::setAddress(const string anAddress){
+	address = anAddress;
+}
+
+// Description: Sets the patient's phone number.
+void Patient::setPhone(const string aPhone){
+	phone = aPhone;
+}
+
+// Description: Sets the patient's email.
+void Patient::setEmail(const string anEmail){
+	email = anEmail;
+}
+
+// Overloaded Operators
+// Description: Comparison operator. Compares "this" Patient object with "rhs" Patient object.
+//              Returns true if both Patient objects have the same care card number.
+bool Patient::operator==(const Patient& rhs){
+	int index;
+	string rhsCareCard = rhs.getCareCard();
+
+	for( index=0; index<10; index++ ){
+		if( careCard[index] != rhsCareCard[index] ){
+			return false;
+		}
+	}
+
+	return true;
+}
+	
+// Description: Greater than operator. Compares "this" Patient object with "rhs" Patient object.
+//              Returns true if the care card number of "this" Patient object is > the care card number of "rhs" Patient object.
+bool Patient::operator>(const Patient & rhs){
+	int index;
+	string rhsCareCard = rhs.getCareCard();
+
+	for( index=0; index<10; index++ ){
+		if( careCard[index] > rhsCareCard[index] ){
+			return true;
+		}
+	}
+
+	return false;
+}
+	
+// Description: Prints the content of "this" patient.
+void Patient::printPatient( ){
+	cout << careCard << ", Patient: " << name << ", " << address << ", " << phone 
+	     << ", " << email << endl;
+}
+
+// End of Patient.cpp
---
+++ WICPS.cpp
@@ -0,0 +1,338 @@
+/*
+ * WICPS.cpp
+ * 
+ * Description: Models a walk-in clinic patient system
+ *				Maintains the information of the patients which can be added,
+ *				edited, or deleted
+ *				Data isn't saved after the program is exited
+ *
+ * Last modified on: May 29, 2017
+ * Author: Vicky Lau
+ */
+
+#include <string>
+#include <iostream>
+#include <sstream>	// For stringstream to convert from string to integer
+#include "List.h"
+#include "Patient.h"
+
+using namespace std;
+
+// Description: Checks if the string only contains integers
+bool isDigit( string aString ){
+	int index = 0;
+
+	while( aString[index] != 0 ){
+		if( aString[index] < '0' || aString[index] > '9' ){
+			return false;
+		}
+
+		index++;
+	}
+
+	return true;
+}
+
+// Description: Returns true if the string is 10 digits
+bool isTenDigits( string aString ){
+	int count = 0;
+
+	while( aString[count] != 0 ){
+		count++;
+	}
+
+	if( count == 10 && isDigit( aString ) ){
+		return true;
+	}
+
+	return false;
+}
+
+// Description: Prompts user for desired action.
+//				Loops until an integer from 1 to arrSize-1 is entered.
+//				Each of those integers correspond to a avaliable option.
+// Precondition: arr[0] is a prompt.
+//				 The rest of the array elements are options.
+// Postcondition: returns an integer from 1 to arrSize-1
+int optionPrompt( string arr[], int arrSize ){
+	int input = 0;
+	string strInput;
+	int index;
+
+	cout << endl;
+	cout << "-----" << endl;
+	cout << arr[0] << endl;
+
+	// Prints all the options
+	for( index = 1; index < arrSize; index++ ){
+		cout << "   " << index << ") " << arr[index] << endl;
+	}
+		
+	cout << endl;
+	cout << " Please enter an option number: ";
+
+	// Continues prompting user until an indicated integer is entered
+	while( input < 1 || input >= arrSize ){
+		getline( cin, strInput );
+
+		// Converts the string strInput to integer input
+		// if strInput only consists of integers
+		if( isDigit( strInput ) ){
+			stringstream convert(strInput);
+			if( !(convert >> input) ){
+				input = 0;
+			}
+		}
+		else{
+			input = 0;
+		}
+
+		if( input < 1 || input >= arrSize ){
+			cout << " Invalid entry. Please enter a number from 1-" << arrSize-1 
+			     << ": ";
+		}
+	}
+
+	cout << endl;
+
+	return input;
+}
+
+// Description: Prompts the user for a care card number.
+string careCardPrompt(){
+	string aCareCard;
+
+	cout << " Please enter the patient's CareCard Number: ";
+	getline( cin, aCareCard );
+
+	return aCareCard;
+}
+
+// Description: Returns true if care card is in list
+// Preconditon: aCareCard is a valid care card number
+bool inList( List list, string aCareCard ){
+	Patient check = Patient( aCareCard );
+
+	if( list.search( check ) != NULL ){ // Meaning it exists in list
+		return true;
+	}
+
+	return false;
+}
+
+// Descriptions: Checks to see if the list has any patients.
+//				 If it's empty, tells the user there are no patients in the system
+bool checkListEmpty( List list ){
+	if( list.getElementCount() > 0 ){
+		return false;
+	}
+
+	cout << " There are currently no patients in the system." << endl;
+	return true;
+
+}
+
+
+int main(void){
+	int 	mainInput 	  = 0;
+	int 	modiInput 	  = 0;
+	string	strInput	  = "";
+	string	initPrompt[8] = {	"What would you like to do?",
+								"Enter a new patient",
+								"Remove a patient",
+								"Search for a patient",
+								"Modify a patient's record",
+								"See list of patients",
+								"Remove all patients",
+								"Exit system" };
+	string  modiPrompt[7] = {	"What would you like to modify?",
+								"Name",
+								"Address",
+								"Phone Number",
+								"E-Mail",
+								"All of the above",
+								"Cancel" };
+	List 	list;
+	Patient entry;
+	Patient *entryPtr;
+
+	const int ADD 		 = 1;
+	const int REMOVE 	 = 2;
+	const int SEARCH 	 = 3;
+	const int MODIFY 	 = 4;
+	const int PRINT 	 = 5;
+	const int REMOVE_ALL = 6;
+	const int QUIT 		 = 7;
+
+	const int MOD_NAME 	  = 1;
+	const int MOD_ADDRESS = 2;
+	const int MOD_PHONE   = 3;
+	const int MOD_EMAIL   = 4;
+	const int MOD_ALL 	  = 5;
+
+	cout << endl;
+	cout << "******* Welcome to the Walk-In Clinic Patient System *******" << endl;
+
+	while( mainInput != QUIT ){
+		mainInput = optionPrompt( initPrompt, 8 );
+
+		// Add a patient
+		if( mainInput == ADD ){ 
+			cout << "Adding a Patient:" << endl;
+
+			strInput = careCardPrompt();
+
+			if( isTenDigits( strInput ) && !inList( list, strInput ) ){
+				entry = Patient( strInput );
+
+				if( list.insert( entry ) ){
+					cout << " Patient " << entry.getCareCard() 
+					     << " has been successfully added." << endl;
+				}
+				else{
+					cout << " Error. System is full." << endl;
+				}
+			}
+			else{
+				cout << " Error. Either the CareCard " << strInput 
+					 << " is invalid or already exists in the system." << endl;
+			}
+			
+		}
+
+		// Remove a patient
+		else if( mainInput == REMOVE ){ 
+			cout << "Removing a Patient:" << endl;
+			if( !checkListEmpty( list ) ){
+				strInput = careCardPrompt();
+
+				if( isTenDigits( strInput ) && inList( list, strInput ) ){
+					entry = Patient( strInput );
+
+					if( list.remove(entry) ){
+						cout << " Patient " << strInput 
+						     << " has been successfully removed." << endl;
+					}
+					else{
+						cout << " Error. The patient was not removed." << endl;
+					}
+				}
+				else{
+					cout << " Error. The CareCard " << strInput
+						 << " does not exist in the system." << endl;
+				}
+			}
+		}
+
+		// Search for a patient
+		else if( mainInput == SEARCH ){ 
+			cout << "Searching for a Patient:" << endl;
+			if( !checkListEmpty( list ) ){
+				strInput = careCardPrompt();
+
+				if( isTenDigits( strInput ) && inList( list, strInput ) ){
+					entry = Patient( strInput );
+					entryPtr = list.search( entry );
+
+					if( entryPtr != NULL ){
+						entry = *entryPtr;
+
+						cout << " ";
+
+						entry.printPatient();
+					}
+					else{
+						cout << " Error. Patient not found." << endl;
+					}
+				}
+				else{
+					cout << " Error. The CareCard " << strInput
+						 << " does not exist in the system." << endl;
+				}
+			}			
+		}
+
+		// Modify a Patient
+		else if( mainInput == MODIFY ){ 
+			cout << "Modifying a Patient:" << endl;
+			if( !checkListEmpty( list ) ){
+				strInput = careCardPrompt();
+
+				if( isTenDigits( strInput ) && inList( list, strInput ) ){
+					entry = Patient( strInput );
+					entryPtr = list.search( entry );
+
+					if( entryPtr == NULL ){
+						cout << " Error. Patient not found.";
+					}
+					else{
+						modiInput = optionPrompt( modiPrompt, 7 );
+
+						// Modify name
+						if( modiInput == MOD_ALL || modiInput == MOD_NAME ){
+							cout << "  New name: ";
+							getline( cin, strInput );
+
+							entryPtr->setName( strInput );
+						}
+			
+						// Modify address
+						if( modiInput == MOD_ALL || modiInput == MOD_ADDRESS ){ 
+							cout << "  New address: ";
+							getline( cin, strInput );
+
+							entryPtr->setAddress( strInput );
+						}
+			
+						// Modify phone
+						if( modiInput == MOD_ALL || modiInput == MOD_PHONE ){ 
+							cout << "  New phone number: ";
+							getline( cin, strInput );
+
+							entryPtr->setPhone( strInput );
+						}
+			
+						// Modify email
+						if( modiInput == MOD_ALL || modiInput == MOD_EMAIL ){ 
+							cout << "  New e-mail: ";
+							getline( cin, strInput );
+
+							entryPtr->setEmail( strInput );
+						}
+					}	
+				}
+				else{
+					cout << " Error. The CareCard " << strInput
+						 << " does not exist in the system." << endl;
+				}
+			}	
+		}
+
+		// Prints a list of patients
+		else if( mainInput == PRINT ){ 
+			cout << "List of Patients:" << endl;
+
+			if( !checkListEmpty( list ) ){
+				list.printList();
+			}	
+		}
+
+		// Removes all patients
+		else if( mainInput == REMOVE_ALL ){
+			cout << "Removing all Patient:" << endl;
+			if( !checkListEmpty( list ) ){
+				list.removeAll();
+
+				cout << " All records of patients have been deleted." << endl;
+			}			
+		}
+	}
+
+	cout << endl;
+	cout << "Thank you for using the Walk-In Clinic Patient System!" << endl;
+	cout << endl;
+
+	return 0;
+}
+
+// End of WICPS.cpp
