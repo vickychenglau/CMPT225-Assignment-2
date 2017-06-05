5f587dce9b09ebf81690ea45d3656a133fd6429f
vcl2 <vcl2@sfu.ca>
2017-06-05 13:56:03 -0700

    List.cpp

---
+++ List.cpp
@@ -0,0 +1,49 @@
+/*
+ * List.cpp
+ * 
+ * Class Description: A value-oriented List data collection ADT.
+ * Class Invariant: Data collection with the following characteristics:
+ *                   - Each element is unique (no duplicates).
+ *                   - It is sorted by ascending sort order of its elements.
+ *                   - Its data structure is expandable: when full, it expands to accommodate 
+ *                     new insertion. This is done unbeknown to the client code.
+ *
+ * Last modified on: June, 2017
+ * Author: Vicky Lau, Jacky Tse
+ */
+
+#include <string>
+#include "Patient.h"
+#include "List.h"
+
+using namespace std;
+
+	// Default constructor
+List::List();
+
+	// Description: Returns the total element count currently stored in List.
+int List::getElementCount() const;
+
+	// Description: Insert an element.
+    //              When "this" List is full, its data structure is expanded to accommodate 
+    //              a new element. This is done unbeknown to the client code.
+	//              If the insertion is successful, true is returned otherwise, false is returned.
+	// Precondition: newElement must not already be in data collection.  
+	// Postcondition: newElement inserted and the appropriate elementCount has been incremented.	
+bool List::insert(const Patient& newElement);
+
+	// Description: Remove an element. 
+	//              If the removal is successful, true is returned otherwise, false is returned.
+	// Postcondition: toBeRemoved is removed, the appropriate elementCount has been decremented.	
+bool List::remove( const Patient& toBeRemoved );
+	
+	// Description: Remove all elements.
+void List::removeAll();
+   
+	// Description: Search for target element and returns a pointer to it if found,
+	//              otherwise, returns NULL.
+Patient* List::search(const Patient& target);
+   
+	// Description: Prints all n elements stored in List in sort order and does so in O(n).
+void List::printList();
+	
